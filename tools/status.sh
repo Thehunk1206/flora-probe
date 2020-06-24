8926417efc0283d80355b71f4a049f3bb8f289cf
predatorx7 <smushaheed@outlook.com>
2020-06-25 05:15:02 +0530

    Improved status tool

--- tools/status.sh
+++ tools/status.sh
@@ -4,10 +4,11 @@ CURRENT_VERSION="$(sed -n -e 's/^.*version: //p' ${MAIN_PUBSPEC})"
 
 echo "Current version: v$CURRENT_VERSION"
 echo "Latest release: $(git describe --abbrev=0)"
-echo ""
-printf "Current developers: \n"
-python tools/support/format_print_list.py .git/refs/remotes/origin/dev
-echo ""
+echo
+printf "Current contributors: \n"
+contributors=$(git shortlog -sne --all)
+echo "$contributors"
+echo
 echo "Flutter project size: $(du -sh ./org.floraprobe)"
 git gc --quiet
 git_object_count=$(git count-objects -vH)
--- tools/support/format_print_list.py
+++
@@ -1,6 +0,0 @@
-from sys import argv
-from os import listdir
-
-x = listdir(argv[1])
-for i in x:
-  print('  '+i)
